02842514d54ed53e36a05cae475ea5b76c04c40f
Wu Shufeng <shufeng.wu@mytuleap.drc-bj.com>
2018-06-14 17:43:37 +0800

    takephoto测试

--- takephototest/src/main/java/com/wsf/test/takephototest/MainActivity.java
+++ takephototest/src/main/java/com/wsf/test/takephototest/MainActivity.java
@@ -1,29 +1,44 @@
 package com.wsf.test.takephototest;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.jph.takephoto.app.TakePhoto;
 import com.jph.takephoto.app.TakePhotoImpl;
 import com.jph.takephoto.model.InvokeParam;
 import com.jph.takephoto.model.TContextWrap;
 import com.jph.takephoto.model.TResult;
+import com.jph.takephoto.model.TakePhotoOptions;
 import com.jph.takephoto.permission.InvokeListener;
 import com.jph.takephoto.permission.PermissionManager;
 import com.jph.takephoto.permission.TakePhotoInvocationHandler;
 
-public class MainActivity extends AppCompatActivity implements TakePhoto.TakeResultListener,InvokeListener {
+import java.io.File;
 
+public class MainActivity extends AppCompatActivity implements TakePhoto.TakeResultListener, InvokeListener, View.OnClickListener {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
     private TakePhoto takePhoto;
     private InvokeParam invokeParam;
+    private Button btnCamera;
+    private Button btnPhotoAlbum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getTakePhoto().onCreate(savedInstanceState);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnCamera = findViewById(R.id.btn_camera);
+        btnCamera.setOnClickListener(this);
+        btnPhotoAlbum = findViewById(R.id.btn_photo_album);
+        btnPhotoAlbum.setOnClickListener(this);
     }
 
     @Override
@@ -40,6 +55,17 @@ public class MainActivity extends AppCompatActivity implements TakePhoto.TakeRes
 
     @Override
     public void takeSuccess(TResult result) {
+        Log.d(TAG, "takeSuccess: " + result.getImages().get(0).getOriginalPath());
+        //System.out.println(result.getImages().get(0).getOriginalPath());
+        notifySystemToScan(result.getImages().get(0).getOriginalPath());
+    }
+
+    public void notifySystemToScan(String filePath) {
+        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File file = new File(filePath);
+        Uri uri = Uri.fromFile(file);
+        intent.setData(uri);
+        getApplication().sendBroadcast(intent);
 
     }
 
@@ -50,17 +76,11 @@ public class MainActivity extends AppCompatActivity implements TakePhoto.TakeRes
 
     @Override
     public void takeCancel() {
-
     }
 
-    /*@Override
-    public PermissionManager.TPermissionType invoke(InvokeParam invokeParam) {
-        return null;
-    }*/
-
-    public TakePhoto getTakePhoto(){
-        if (takePhoto==null){
-            takePhoto= (TakePhoto) TakePhotoInvocationHandler.of(this).bind(new TakePhotoImpl(this,this));
+    public TakePhoto getTakePhoto() {
+        if (takePhoto == null) {
+            takePhoto = (TakePhoto) TakePhotoInvocationHandler.of(this).bind(new TakePhotoImpl(this, this));
         }
         return takePhoto;
     }
@@ -69,16 +89,41 @@ public class MainActivity extends AppCompatActivity implements TakePhoto.TakeRes
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         //以下代码为处理Android6.0、7.0动态权限所需
-        PermissionManager.TPermissionType type=PermissionManager.onRequestPermissionsResult(requestCode,permissions,grantResults);
-        PermissionManager.handlePermissionsResult(this,type,invokeParam,this);
+        PermissionManager.TPermissionType type = PermissionManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        PermissionManager.handlePermissionsResult(this, type, invokeParam, this);
     }
 
     @Override
     public PermissionManager.TPermissionType invoke(InvokeParam invokeParam) {
-        PermissionManager.TPermissionType type=PermissionManager.checkPermission(TContextWrap.of(this),invokeParam.getMethod());
-        if(PermissionManager.TPermissionType.WAIT.equals(type)){
-            this.invokeParam=invokeParam;
+        PermissionManager.TPermissionType type = PermissionManager.checkPermission(TContextWrap.of(this), invokeParam.getMethod());
+        if (PermissionManager.TPermissionType.WAIT.equals(type)) {
+            this.invokeParam = invokeParam;
         }
         return type;
+    }
+
+    @Override
+    public void onClick(View v) {
+        File file = new File(Environment.getExternalStorageDirectory(), "/temp/" + System.currentTimeMillis() + ".jpg");
+        if (!file.getParentFile().exists()) {
+            file.getParentFile().mkdirs();
+        }
+        Uri imageUri = Uri.fromFile(file);
+
+        //使用自带相册
+        TakePhotoOptions.Builder builder = new TakePhotoOptions.Builder();
+            builder.setWithOwnGallery(true);
+        takePhoto.setTakePhotoOptions(builder.create());
+
+        switch (v.getId()) {
+            case R.id.btn_camera:
+                takePhoto.onPickFromCapture(imageUri);
+                break;
+            case R.id.btn_photo_album:
+                takePhoto.onPickFromGallery();
+                break;
+            default:
+                break;
+        }
     }
 }
